bf68ebb3a9e94197fb0fe0441804b24d773a363f
Joseph Yika <josephyika123@gmail.com>
2022-04-18 14:06:53 +0200

    Adding files

---
+++ Scripts/script.js
@@ -0,0 +1,10 @@
+
+var TypedText = new Typed
+(".animatedText", {
+    strings: ["STUDENT - COMPUTING", "ASPIRING DEVELOPER AND ENGINEER", "BUT ALSO", "PIANO TEACHER AND PIANIST"],
+    typeSpeed: 40,
+    backSpeed: 40,
+    loop: true
+
+});
+
