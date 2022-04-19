4359ca60d117486f3d22d040be041957bc90dcc4
Joseph Yika <josephyika123@gmail.com>
2022-04-19 08:17:49 +0200

    Updated all files

--- Scripts/script.js
+++ Scripts/script.js
@@ -8,3 +8,9 @@ var TypedText = new Typed
 
 });
 
+window.addEventListener('scroll', function(){
+    var header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 0)
+
+})
+
