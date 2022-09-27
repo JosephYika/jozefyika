46a862b2a59525d0f2742b354df2d6fd8ab2b9d2
Joseph Yika <josephyika123@gmail.com>
2022-09-27 11:20:47 +0200

    Updated Website

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,5 +1,5 @@
 
-var TypedText = new Typed
+var TypedText = new Typed // assign new Object o a variable TypedText
 (".animatedText", {
     strings: ["STUDENT - COMPUTING", "ASPIRING DEVELOPER AND ENGINEER", "BUT ALSO", "PIANO TEACHER AND PIANIST"],
     typeSpeed: 40,
@@ -8,8 +8,8 @@ var TypedText = new Typed
 
 });
 
-window.addEventListener('scroll', function(){
-    var header = document.querySelector('header');
+window.addEventListener('scroll', function(){ // attach an event handler to the specified element - in this case scroll
+    var header = document.querySelector('header'); // return the first element
     header.classList.toggle('sticky', window.scrollY > 0)
 
 })
